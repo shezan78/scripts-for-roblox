16fc7196e0ab78c8354369257948c22d4ee30586
shezan78 <97002070+shezan78@users.noreply.github.com>
2022-12-24 12:30:13 -0500

    Create ahahahah.lua

---
+++ ahahahah.lua
@@ -0,0 +1,15 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/scripts-for-roblox/main/asdlolblobprotected.lua", true))()
+getgenv().DaHoodSettings.Prediction = 0.123
+getgenv().Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftFoot", "RightFoot", "UpperTorso",}
+getgenv().Aiming.FOV = 20
+getgenv().Aiming.FOVSides = 25
+getgenv().Aiming.HitChance = 101
+getgenv().Aiming.ShowFOV = false
+
+getgenv().DaHoodSettings.AUTO_PREDICTION = true
+
+getgenv().Aimlock = true
+getgenv().AimlockKey = "x" -- aimbot toggle
+getgenv().AimPart = "UpperTorso" -- UpperTorso, LowerTorso, HumanoidRootPart, Head
+getgenv().PredictionVelocity = 6.612 -- aimbot prediction.
+getgenv().SmoothnessAmount = 0.0081 -- dont go over 0.01 to look legit
