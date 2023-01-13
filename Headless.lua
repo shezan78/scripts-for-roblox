58907a3397105e7a5511d6953bd0bc87468aa288
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-12 19:54:36 -0500

    Create Headless.lua

---
+++ Headless.lua
@@ -0,0 +1,5 @@
+game.Players.LocalPlayer.Character.Head.Size = Vector3.new(0,0, 0)
+    game.Players.LocalPlayer.Character.Head.Size = Vector3.new(0,0, 0)
+    game.Players.LocalPlayer.Character.Head.Massless = true
+    game.Players.LocalPlayer.Character.Head.Massless = true
+--headless
