bb923b8f7659bd31fc4964ff3f7796db79bbffd3
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-24 21:26:09 -0500

    Update script for khurshed.lua

--- script for khurshed.lua
+++ script for khurshed.lua
@@ -41,14 +41,14 @@ local Button = Tab:CreateButton({
 local Button = Tab:CreateButton({
 	Name = "Pink dot lock Keybind = Q or C",
 	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/Pink%20Dot%20lock%201.lua", true))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/Pink%20Dot%20Lock%202.lua", true))()
 	end,
 })
 
 local Button = Tab:CreateButton({
 	Name = "Pink dot lock 2 Keybind = Q",
 	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/shezans2wsalt/addontokhurshedsscript/main/Pink%20dot%20lock%20script2.lua", true))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/Pink%20Dot%20lock%201.lua", true))()
 	end,
 })
 
